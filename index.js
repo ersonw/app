bf1d2601f478188335bcb5270accf1fdc1550a24
ersonw <asoonnode@gmail.com>
2022-10-05 21:35:24 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+import {AppRegistry} from 'react-native';
+import RootStackScreen from './src/navigation/RootStackScreen';
+import {name as appName} from './app.json';
+
+// 图标库
+import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import EvilIcons from 'react-native-vector-icons/EvilIcons';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+
+AppRegistry.registerComponent(appName, () => RootStackScreen);
+
+SimpleLineIcons.loadFont();
+Ionicons.loadFont();
+EvilIcons.loadFont();
+MaterialIcons.loadFont();
---
+++ src/navigation/options/TabOption.js
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons';
+
+// TabOption 配置
+const TabOption = route => {
+  let labelName;
+  let iconName;
+
+  switch (route.name) {
+    case 'DiscoverStack':
+      labelName = '发现';
+      iconName = 'compass';
+      break;
+    case 'VideoStack':
+      labelName = '视频';
+      iconName = 'camrecorder';
+      break;
+    default:
+      labelName = '我的';
+      iconName = 'user';
+  }
+  return {
+    tabBarLabel: labelName,
+    tabBarIcon: ({focused, color}) => (
+      <SimpleLineIcons name={iconName} size={25} color={color} />
+    ),
+  };
+};
+
+export default TabOption;
